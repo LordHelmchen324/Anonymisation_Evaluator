4950f7c43f5ba570cb8f6a3b6e83d8bc9f5ff006
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-05-13 19:16:23 +0200

    Wrote basic main function for actual evaluation

--- Evaluator.java
+++ Evaluator.java
@@ -1,7 +1,13 @@
 public class Evaluator {
 
 	public static void main(String[] args) {
-		System.out.println("I am an evaluator!");
+		Dataset o = Dataset.fromJSON(args[1]);
+		Dataset p = Dataset.fromJSON(args[2]);
+
+		double dr = DisclosureRisk.computeViaRecordLinkage(o, p);
+		double il = InformationLoss.compute(o, p);
+		
+		Plotter.plotRUMap(dr, il);
 	}
 
 }
