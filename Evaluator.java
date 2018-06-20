ab658f1061f5344318c634d1832e7ed1b4215dfb
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-06-20 20:57:40 +0200

    Added CSV output for the evaluation results

--- Evaluator.java
+++ Evaluator.java
@@ -1,3 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Evaluator {
 
 	public static void main(String[] args) {
@@ -7,15 +16,19 @@ public class Evaluator {
 
 		// Artifical data set
 
-		Dataset o1 = Dataset.fromJSON("../Perfect Data Set/perfect_grid_1000_60_2000_2000.json");
+		Dataset o = Dataset.fromJSON("../Perfect Data Set/perfect_grid_1000_60_2000_2000.json");
+
+		List<Evaluator.ResultRecord> results = new LinkedList<Evaluator.ResultRecord>();
 
 		for (String dMString : distanceMeasureStrings) {
 			for (String mSString : medianStrategyStrings) {
 				for (int k : ks) {
 					Dataset p = Dataset.fromJSON("../Perfect Data Set/perfect_grid_1000_60_2000_2000_PROTECTED_" + dMString + "_" + mSString + "_k" + k + ".json");
 
-					double dr = DisclosureRisk.computeViaRecordLinkage(o1, p);
-					double il = InformationLoss.compute(o1, p);
+					double dr = DisclosureRisk.computeViaRecordLinkage(o, p);
+					double il = InformationLoss.compute(o, p);
+
+					results.add(new Evaluator.ResultRecord(dMString, mSString, k, dr, il));
 
 					System.out.println("Disclosure risk: " + dr);
 					System.out.println("Information loss: " + il);
@@ -23,22 +36,62 @@ public class Evaluator {
 			}
 		}
 
+		Evaluator.ResultRecord.resultsToFile(results, "../Perfect Data Set/evaluation_results.csv");
+
 		// Real data set
 
-		Dataset o2 = Dataset.fromJSON("../San Francisco Taxi Data/CabSpotting_pandas-downsampled.json");
+		o = Dataset.fromJSON("../San Francisco Taxi Data/CabSpotting_pandas-downsampled.json");
+
+		results = new LinkedList<Evaluator.ResultRecord>();
 
 		for (String mSString : medianStrategyStrings) {
 			for (int k : ks) {
 				Dataset p = Dataset.fromJSON("../San Francisco Taxi Data/CabSpotting_pandas-downsampled_PROTECTED_sync_" + mSString + "_k" + k + ".json");
 
-				double dr = DisclosureRisk.computeViaRecordLinkage(o2, p);
-				double il = InformationLoss.compute(o2, p);
+				double dr = DisclosureRisk.computeViaRecordLinkage(o, p);
+				double il = InformationLoss.compute(o, p);
+
+				results.add(new Evaluator.ResultRecord("sync", mSString, k, dr, il));
 
 				System.out.println("Disclosure risk: " + dr);
 				System.out.println("Information loss: " + il);
 			}
 		}
+
+		Evaluator.ResultRecord.resultsToFile(results, "../San Francisco Taxi Data/evaluation_results.csv");
 		
+	}
+
+	private static class ResultRecord {
+		public String distanceMeasureString = "None";
+		public String medianStrategyString = "None";
+		public int k = -1;
+
+		public double dr = -1.0;
+		public double il = -1.0;
+
+		public ResultRecord(String distanceMeasureString, String medianStrategyString, int k, double dr, double il) {
+			this.distanceMeasureString = distanceMeasureString;
+			this.medianStrategyString = medianStrategyString;
+			this.k = k;
+			this.dr = dr;
+			this.il = il;
+		}
+
+		public static void resultsToFile(List<ResultRecord> results, String filePath) {
+			File resultsFile = new File(filePath);
+			try (BufferedWriter w = new BufferedWriter(new FileWriter(resultsFile))) {
+				for (Evaluator.ResultRecord result : results) {
+					w.write(result.distanceMeasureString + "," + result.medianStrategyString + "," + result.k + "," + result.dr + "," + result.il + "\n");
+				}
+			} catch (FileNotFoundException e) {
+				System.err.println("Could not find file at path \"" + resultsFile.getName() + "\".");
+				System.exit(1);
+			} catch (IOException e) {
+				System.err.println("An I/O exception occured: " + e.getLocalizedMessage());
+				System.exit(1);
+			}
+		}
 	}
 
 }
